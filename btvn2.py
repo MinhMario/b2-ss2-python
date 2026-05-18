42a1f1aabf12a531dbe833c69ea71d9e6a803939
MinhMario <minom35w@gmail.com>
2026-05-18 08:16:03 +0700

    Create btvn2.py

---
+++ btvn2.py
@@ -0,0 +1,10 @@
+print (" -- HỆ THỐNG NHẬP CHỈ SỐ SINH TÔN --- ")
+name_patient = input ("Nhập tên bệnh nhân : ")
+weight =float( input ("Nhập cân nặng bệnh nhân : "))
+
+print (" --- KIỂM TRA DỮ LIỆU LƯU TRỮ ---")
+print ("Bệnh nhân : " , name_patient)
+print ("Cân nặng dã nhập : " , weight)
+
+# Trường nhom IT viet thêm dòng này dể kiểm tra dữ liệu của cân nặng
+print ("CẢNH BÁO - Kiểu dữ liệu dang lưu là : ", type(weight))
